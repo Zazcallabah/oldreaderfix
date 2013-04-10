5c784fe1d2e267082888ca7e624be31ca540ff51
Peter Hamberg <github@prefect.se>
2013-04-10 05:20:11 +0300

    Update r.js

--- r.js
+++ r.js
@@ -1,16 +1,23 @@
- function hide()
+ function block()
  {
-	$("li.sort-item > a > span.hidden").each( function(){
-	$("#"+this.parentNode.parentNode.id).hide();
+	$("a.read").each( function(){
+	var x = this.outerHTML;
+	var i1 = x.indexOf("data-confirm");
+	if( i1 !== -1 )
+	{
+	var s1 = x.substring(0,i1);
+	var s2 = x.substring(i1+6);
+	this.outerHTML= s1+s2;
+	}
 	});
  }
 function DOMModifiedHandler(){
   $(this).unbind('DOMSubtreeModified');
     setTimeout(function(){
-        hide();
-        $('#sidebar').bind('DOMSubtreeModified',DOMModifiedHandler);
+        block();
+        $('#main').bind('DOMSubtreeModified',DOMModifiedHandler);
     },1000);
 }
 
-$('#sidebar').bind('DOMSubtreeModified',DOMModifiedHandler);
-hide();
+$('#main').bind('DOMSubtreeModified',DOMModifiedHandler);
+block();
